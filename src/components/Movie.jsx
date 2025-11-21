20f96026a3828a9bfc4b35f1b29d809c16740ed3
ZachJ823 <157136042+ZachJ823@users.noreply.github.com>
2025-11-21 14:49:58 -0600

    Add skeleton of favorite/watched state management

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,8 +5,26 @@ import { useState } from "react";
 // Use https://api.themoviedb.org/3/movie/{movie_id}.
 
 // Utilizes both the MovieBasic and MovieAdv components to encapsulate an entire movie component.
+// Tagline is not implemented yet. Depends on the final design.
 export default function Movie(props) {
     const { title, year, runtime, rated, genre, poster, tagline, plot, director, actors, rt_rating, i_rating, mc_rating } = props
+
+    const [watched, setWatched] = useState(false);
+    const [favorited, setFavorited] = useState(false);
+
+
+    // movieWatched and movieFavorited are stored locally.
+    // Function to toggle movies as watched
+    const movieWatched = () =>
+    {
+        setWatched(!watched);
+    }
+
+    // Function to toggle movies are favorited
+    const movieFavorited = () =>
+    {
+        setFavorited(!favorited);
+    }
 
     return (
 
