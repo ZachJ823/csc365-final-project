363f1cf581f91339fcd712f86cbc57f743fc1a67
ZachJ823 <157136042+ZachJ823@users.noreply.github.com>
2025-11-21 14:48:19 -0600

    Add rating component to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Movie from './components/Movie';
+import Rating from './components/Rating';
 
 function App() {
 
   return (
-  <div>  
+  <div> 
   </div>
   )
 }
