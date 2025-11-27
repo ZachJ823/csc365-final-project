728df1ec5b2268f0f5f0612180d67cedbcf29155
ZachJ823 <157136042+ZachJ823@users.noreply.github.com>
2025-11-27 11:42:02 -0600

    "Fix" search. It works, but CORS hates that fact.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 
-// Searching is not totally implemented. Primarily focused on the form. No results are yielded yet.
+// Wow I hate react and javascript. And the web. CORS is truly one of the most amazing things.
 export default function Search() {
     const [search, setSearch] = useState('');
     const [results, setResults] = useState([])
 
-    const searchMovie = async (e) => {
+    const searchMovie = () => {
         const options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${import.meta.env.REACT_APP_AUTH}`,
-                api_key: `${import.meta.env.REACT_API_KEY}`
+                Authorization: `Bearer ${import.meta.env.API_AUTH}`,
+                api_key: import.meta.env.API_KEY
             }
         };
 
-        await fetch(`https://api.themoviedb.org/3/search/movie?query=${search}include_adult=false&language=en-US&page=1`, options)
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`, options)
             .then(res => res.json())
             .then(res => console.log(res))
             .catch(err => console.error(err));
     };
 
     return (
-        <div>            
-        <input
-        type="text"
-        name="searchinput"
-        required={true}
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search for a movie."
-        />
-        <button
-        onClick={searchMovie}/>
+        <div>
+            <input
+                type="text"
+                name="searchinput"
+                required={true}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for a movie."
+            />
+            <button onClick={searchMovie}>
+                Search
+            </button>
         </div>
     )
 }
