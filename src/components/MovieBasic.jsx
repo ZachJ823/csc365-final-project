dbc7e1d0d04bd0bc08e47a9f5cd62a8e898fa01b
ZachJ823 <157136042+ZachJ823@users.noreply.github.com>
2025-11-30 00:19:14 -0600

    Add poster

--- src/components/MovieBasic.jsx
+++ src/components/MovieBasic.jsx
@@ -3,11 +3,11 @@ import React from "react";
 // Use https://api.themoviedb.org/3/movie/{movie_id}.
 
 // Component holding basic information on a given movie
-const MovieBasic = ({ title, year, runtime, rated, genre, poster }) => (
+const MovieBasic = ({ title, year, rated, genre, poster }) => (
     <div>
         <p>{title} | {year} | {rated}</p>
-        <p>{runtime} | {genre} </p>
-        <p>{poster}</p>
+        <img className="poster" width="250px" src={`https://image.tmdb.org/t/p/w500/${poster}`}></img>
+        <p> {genre} </p>
     </div>
 );
 
